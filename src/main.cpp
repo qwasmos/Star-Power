b54e86781a35617376eee4a5e7b519aeca0d5b46
Cody Caraballo <qwasmos0102@gmail.com>
2023-06-30 15:46:48 -0400

    I have added the basic structure of the Song object

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
 
+struct Song{
+	std::string title;
+	std::string artist;
+	std::string genre;
+	int length;
+	int BPM;
+	Song(std::string title = "", std::string artist ="",std::string genre = "", int length = 0, int BPM = 0){
+		this->title = title;
+		this->artist = artist;
+		this->genre = genre;
+		this->length = length;
+		this->BPM = BPM;
+	}
+	
+	
+	
+	
+}
+
+
+
+Song Search(Song& ref){
+	// do get request for song name
+	
+	Song song;
+	return song;
+}
+
 int main(){
 
     std::cout<<"Hello Star Power!!!!"<<std::endl;
