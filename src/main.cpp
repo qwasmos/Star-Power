4456d0f772a35b9e29e0009ae11c34ec0de3f959
Cody Caraballo <qwasmos0102@gmail.com>
2023-07-07 23:37:30 -0400

    I have added some very basic functionality of the hash table

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#define Table_Size 250000
+#include <cmath>
 
 struct Song{
 	std::string title;
@@ -20,17 +21,66 @@ struct Song{
 	
 };
 
-
-
-Song Search(Song& ref){
-	// do get request for song name
+int Hash(std::string ref){
+	int index = 0;
+	for(int i =0; i < ref.size();i++){
+		index+=ref[i]* std::pow(2,i);
+	}
 	
-	Song song;
-	return song;
+	return index % Table_Size;
 }
+
+
+struct HashTable{
+	Song* songs[Table_Size];
+	int size;
+	public:
+	HashTable(){
+		for(int i = 0; i < Table_Size;i++){
+			songs[i] = nullptr;
+		}
+		size = 0;
+	}
+	
+	int getSize(){
+		return size;
+	}
+	
+	void Insert(Song& ref){
+		int index = Hash(ref.title);
+		if(songs[index]==nullptr){
+			songs[index] = new Song(ref.title, ref.artist, ref.genre, ref.length, ref.BPM);
+			return;
+		}
+		
+		std::cout<<"The song can not be inserted, it already exists"<<std::endl;
+	}
+	
+	void Search(std::string ref){
+		int index = Hash(ref);
+		if(songs[index] == nullptr){
+			std::cout<<ref<<" does not exist"<<std::endl;
+			return;
+		}
+		
+		std::cout<<ref<<" is here"<<std::endl;
+	}
+	
+	
+};
+
+
+// This will be for the hash table
+
 
 int main(){
 
     std::cout<<"Hello Star Power!!!!"<<std::endl;
+	std::cout<<"jacob ladder"<<std::endl;
+	HashTable ht;
+	Song song("Single Ladies", "Beyonce", "Pop", 125, 225);
+	ht.Insert(song);
+	ht.Search("bacon and eggs");
+	ht.Search("Single Ladies");
 
 }
