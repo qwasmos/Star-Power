1e7d4c75bd3c6b8b29852cf0a5eb9d7aecc7793d
ryanhulke <61066664+ryanhulke@users.noreply.github.com>
2023-07-28 23:11:53 -0500

    fixed input formatting bugs

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Star-Power
-
+I chopped up the data.csv file to make it easier to parse, and removed a few of the useless features (mode, key, release date, speechiness)
+and saved it into "refinedData.csv"
+- i think the input formatting is working correctly and we can create all 65,000 song objects seamlessly. takes about 20 seconds to run
 TODO:
-- fix formatting bugs
-- figure out graph implementation
+- figure out how to add songs to graph and add edges
 - figure out how to compare songs to see if they're similar enough to be connected
--- src/Song.cpp
+++ src/Song.cpp
@@ -1,10 +1,10 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#pragma once
 using namespace std;
 
 class Song {
-    public:
     float valence;
     int year;
     float acousticness;
@@ -15,17 +15,14 @@ class Song {
     bool explicit_;
     string id;
     float instrumentalness;
-    int key;
     float liveness;
     float loudness;
-    int mode;
     string name;
     int popularity;
-    string release_date;
-    float speechiness;
     float tempo;
+    public:
     Song(float _valence, int _year, float _acousticness, vector<string> _artists, float _danceability, int _duration_ms, float _energy, bool _explicit_, string _id, float _instrumentalness,
-     int _key, float _liveness, float _loudness, int _mode, string _name, int _popularity, string _release_date, float _speechiness, float _tempo){
+     float _liveness, float _loudness, string _name, int _popularity, float _tempo){
         valence = _valence;
         year = _year;
         acousticness = _acousticness;
@@ -36,22 +33,19 @@ class Song {
         explicit_ = _explicit_;
         id = _id;
         instrumentalness = _instrumentalness;
-        key = _key;
         liveness = _liveness;
         loudness = _loudness;
-        mode = _mode;
         name = _name;
         popularity = _popularity;
-        release_date = _release_date;
-        speechiness = _speechiness;
         tempo = _tempo;
     }
     bool areSimilar(Song other){
         // work in progress
         // decide on which attributes are important, how to come up with a similarity score, and what the threshold for similarity is
         int similarity = 0;
+
         int diff = abs(valence - other.valence);
-        (diff < .15) ? similarity += pow(1 - diff, 2) : similarity += 0; // if they are within .15 of each other, add 1 - diff to similarity
+        (diff < .15) ? similarity += pow(1 - diff, 2) : similarity += 0; // if they are within .15 of each other, add the square of 1 - diff to similarity
         diff = abs(acousticness - other.acousticness);
         (diff < .15) ? similarity += pow(1 - diff, 2) : similarity += 0;
         return similarity > 1.5;
