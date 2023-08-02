00c0db5273abb943ff0a8a122b99623aca69834e
ryanhulke <61066664+ryanhulke@users.noreply.github.com>
2023-08-02 15:42:20 -0500

    broken

---
+++ src/Hash.cpp
@@ -0,0 +1,11 @@
+
+#include "Song.cpp"
+#include <string>
+#include <functional>
+using namespace std;
+
+class Hash {
+public: size_t operator() (const Song& song) const {
+    return hash<string>()(song.name);
+    }
+};
